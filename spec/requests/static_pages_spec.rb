45584ebfa91042d80ecf3b9e5622da4d38b2926c
Elad Sofer <elad.sofer@gmail.com>
2013-02-21 18:14:10 +0200

    Add about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,19 @@ describe "StaticPages" do
 	page.should have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About us'" do
+      visit '/static_pages/about'
+      page.should have_content('About us')
+    end
+  end
 end
