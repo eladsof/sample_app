8adc6b7d9aa7c5569595e61f6fb9ff42c58cc978
Elad Sofer <elad.sofer@gmail.com>
2013-03-19 16:00:51 +0200

    Refactor tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -20,3 +20,27 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_title do |message|
+  match do |page|
+    page.should have_selector('title', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_text_header do |message|
+  match do |page|
+    page.should have_selector('h1', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_profile_link do |url|
+  match do |page|
+    page.should have_link('Profile', url: url)
+  end
+end
+RSpec::Matchers.define :have_signin_link do |url|
+  match do |page|
+    page.should have_link('Sign in', url: url)
+  end
+end
+
